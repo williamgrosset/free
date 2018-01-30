37493d54c1410b4a4a3a04276643480c510223f6
William Grosset <williamhgrosset@gmail.com>
2018-01-30 15:30:45 -0800

    Initial welcome

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">William Grosset</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Writings</li>
+          <li>Contact</li>
+        </ul>
       </div>
     );
   }
