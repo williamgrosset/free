6f09f6807c01a79525569fd48979a5240c490635
William Grosset <williamhgrosset@gmail.com>
2018-01-30 15:32:32 -0800

    Remove app logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +6,6 @@ class App extends Component {
     return (
       <div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">William Grosset</h1>
         </header>
         <ul>
