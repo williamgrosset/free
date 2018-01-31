dafc23d198266b098ac247c56712ff2a2f41b1bd
William Grosset <williamhgrosset@gmail.com>
2018-01-30 21:26:03 -0800

    Introduction component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
+import Introduction from './components/Introduction';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="header">
-          <h1 className="title">William</h1>
-          <h1 className="title">Grosset</h1>
-        </div>
+        <Introduction />
         <Navigation />
       </div>
     );
---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Introduction extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1 className="title">William</h1>
+        <h1 className="title">Grosset</h1>
+      </div>
+    );
+  }
+}
+
+export default Introduction;
