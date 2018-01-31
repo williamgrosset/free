750ab39aa388aa5cdc9e53833826533740a1b2d6
William Grosset <williamhgrosset@gmail.com>
2018-01-30 21:34:29 -0800

    Stylesheets for Intro component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 import Introduction from './components/Introduction';
-import './App.css';
 
 class App extends Component {
   render() {
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Introduction.css';
 
 class Introduction extends Component {
   render() {
