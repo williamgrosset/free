f943046523a638209f4bb5360f26be0cd0180950
William Grosset <williamhgrosset@gmail.com>
2018-02-06 11:44:16 -0800

    Update new About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,24 +2,27 @@ import React from 'react';
 import '../styles/components/About.css';
 
 const About = () => { 
-  const changeLink = "https://www.change.org";
-  const uvicLink = "https://www.google.ca/maps/place/University+of+Victoria/@49.6298867,-116.7198961,5z/data=!4m5!3m4!1s0x548f71575d850fbd:0xc039d0b85e1ede74!8m2!3d48.4634067!4d-123.3116935kadding-top: 5em";
+  const changeURL = "https://www.change.org";
+  const uvicURL = "https://www.google.ca/maps/place/University+of+Victoria/@49.6298867,-116.7198961,5z/data=!4m5!3m4!1s0x548f71575d850fbd:0xc039d0b85e1ede74!8m2!3d48.4634067!4d-123.3116935kadding-top: 5em";
+  const facebookURL = "https://www.facebook.com/WilliamGrosset";
+  const twitterURL = "https://twitter.com/williamhgrosset";
+  const linkedinURL = "https://www.linkedin.com/in/williamgrosset";
+  const githubURL = "https://github.com/williamgrosset";
+  const resumeURL = "https://docs.google.com/document/d/1vaoFvK_89JEpctlfIfrw-Mb2VMdot88AVl7Mdowei44";
 
   return (
     <div className="about">
-      <h2 className="header">My name is William Hamilton Grosset.</h2>
-      <p>
+      <h2 className="header code-font">William Hamilton Grosset.</h2>
+      <p className="sub-about">
         I am currently studying Computer Science
-        at the <a href={uvicLink}>University of Victoria</a>. During 2017, I worked as a research assistant at the
-        University and as a software engineer at <a href={changeLink}>Change.org</a>. I love to read, write, code, and stay active. My interests are in distributed
+        at the <a href={uvicURL}>University of Victoria</a>. During 2017, I worked as a research assistant at the
+        University and as a software engineer at <a href={changeURL}>Change.org</a>. I love to read, write, <span className="code-font">code</span>, and stay active. My interests are in distributed
         systems, machine learning, and building beautiful interfaces.
+        You can find me on <a href={facebookURL}>Facebook</a>, <a href={twitterURL}>Twitter</a>, <a href={linkedinURL}>LinkedIn</a>, <a href={githubURL}>GitHub</a>. Here is my <a href={resumeURL}>resume</a>.
       </p>
-      <p>
+      <p className="sub-about">
         I believe in hard work and persistence.<br />I believe in
         relentlessly pursuing your goals.<br />I believe in transforming everything around you.
-      </p>
-      <p>
-        You can find me on Facebook, Twitter, LinkedIn, GitHub, HackerNews, and here is my resume.
       </p>
     </div>
   );
