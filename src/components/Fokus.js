4b478adb913b08b389b21525412a620d14f4239a
William Grosset <williamhgrosset@gmail.com>
2018-02-12 19:57:40 -0800

    Logos added and positioned

--- src/components/Fokus.js
+++ src/components/Fokus.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/components/Fokus.css';
 import fokusBrowser from '../images/fokus_browser.png';
+import fokusLogo from '../images/fokus_logo.png';
+import googleLogo from '../images/google_logo.png';
 
 const Fokus = () => { 
   return (
     <div className="project">
       <div className="fk-content">
         <img className="fk-photo" src={fokusBrowser} alt='Fokus' />
+        <img className="fk-logo" src={fokusLogo} alt='Fokus' />
+        <img className="google-logo" src={googleLogo} alt='Fokus' />
       </div>
       <div className="fk-sub-content">
         <p className="fk-title">
