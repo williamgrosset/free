8c78503b6b2f111c1c4d80d899c7f0061a6c0005
William Grosset <williamhgrosset@gmail.com>
2018-02-17 14:10:15 -0800

    Add OSCAR link; increase line height

--- src/components/projects/AdditionalProjectContainer.js
+++ src/components/projects/AdditionalProjectContainer.js
@@ -3,13 +3,14 @@ import '../../styles/components/projects/AdditionalProjectContainer.css';
 import ProjectDetailsList from './ProjectDetailsList';
 
 const AdditionalProjectContainer = () => {
+  const oscarURL = 'http://oscarcanada.org/';
   const reactURL = 'https://reactjs.org/';
 
   return (
     <div className="additional-project-container">
       <div className="additional-project">
         <p className="project-title code-font">oscar</p>
-        <p className="project-subtitle additional-sub">Contribution to the open source clinical application.</p>
+        <p className="project-subtitle additional-sub">Automated auditing for the open source medical application <a href={oscarURL}>OSCAR</a>.</p>
         <ProjectDetailsList languages={["source", "Java"]} position="oscar-details-pos" sourceURL="https://github.com/williamgrosset/OSCAR-ConCert" />
       </div>
       <div className="additional-project">
