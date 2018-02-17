a6c1de4d9b7b01aedfce37c9c6ed74574e6bd8fc
William Grosset <williamhgrosset@gmail.com>
2018-02-17 02:00:28 -0800

    Nit: clean-up About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../styles/components/About.css';
 
 const About = () => { 
-  const changeURL = "https://www.change.org";
   const uvicURL = "https://www.google.ca/maps/place/University+of+Victoria/@49.6298867,-116.7198961,5z/data=!4m5!3m4!1s0x548f71575d850fbd:0xc039d0b85e1ede74!8m2!3d48.4634067!4d-123.3116935kadding-top: 5em";
+  const changeURL = "https://www.change.org";
 
   return (
     <div className="about">
