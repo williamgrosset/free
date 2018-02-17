4dcebd443f985211f7c2993923d53ea045b7ba17
William Grosset <williamhgrosset@gmail.com>
2018-02-16 23:31:20 -0800

    Add social links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,11 @@ import React from 'react';
 import '../styles/components/Contact.css';
 
 const Contact = () => { 
-  const emailURL = "https://www.change.org";
+  const emailURL = 'https://www.change.org';
+  const twitterURL = 'https://twitter.com/williamhgrosset';
+  const linkedinURL = 'https://www.linkedin.com/in/williamgrosset/';
+  const hackernewsURL = 'https://news.ycombinator.com/user?id=williamgrosset';
+  const githubURL = 'https://github.com/williamgrosset';
 
   return (
     <div className="contact">
@@ -10,10 +14,10 @@ const Contact = () => {
         You can reach me at<br />
         williamhgrosset@gmail.com<br /><br /><br />
 
-        Twitter.
-        LinkedIn.
-        Hacker News.
-        GitHub.
+        <a href={twitterURL}>Twitter.</a>&nbsp;
+        <a href={linkedinURL}>LinkedIn.</a>&nbsp;
+        <a href={hackernewsURL}>Hacker News.</a>&nbsp;
+        <a href={githubURL}>GitHub.</a>
       </p>
     </div>
   );
