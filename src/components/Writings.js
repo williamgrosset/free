b48c23e1278014f28a66583e7adff6adca36e3e3
William Grosset <williamhgrosset@gmail.com>
2018-02-26 13:10:50 -0800

    Add barebone Writings component

---
+++ src/components/Writings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/components/Writings.css';
+
+const Writings = () => { 
+  return (
+    <div className="writings">
+      <p className="writings-desc">
+        Writings
+      </p>
+    </div>
+  );
+}
+
+export default Writings;
