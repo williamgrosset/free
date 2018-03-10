994ef7b7d3be32bfd38929e78927fa6567928c90
William Grosset <williamhgrosset@gmail.com>
2018-03-10 10:43:52 -0800

    Update uvic link

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/components/About.css';
 
 const About = () => { 
-  const uvicURL = 'https://www.google.ca/maps/place/University+of+Victoria/@49.6298867,-116.7198961,5z/data=!4m5!3m4!1s0x548f71575d850fbd:0xc039d0b85e1ede74!8m2!3d48.4634067!4d-123.3116935kadding-top: 5em';
+  const uvicURL = 'https://en.wikipedia.org/wiki/University_of_Victoria'
   const changeURL = 'https://www.change.org';
 
   return (
