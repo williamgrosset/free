bcf8f51e938be137301ad0ea3c5ff919966d131b
William Grosset <williamhgrosset@gmail.com>
2018-03-12 21:31:12 -0700

    Add project description

--- src/components/projects/Change.js
+++ src/components/projects/Change.js
@@ -8,6 +8,12 @@ const Change = () => (
   <div className="project">
     <div className="ch-content">
       <img className="ch-photo" src={betterWorldPhoto} alt="Better world" />
+      <span className="ch-desc">
+        <p className="ch-desc-txt">
+          Collabored on the revenue engineering team. Initially, I begun working on low priority assignments and
+          quickly moved to high priority features, bug fixes, and daily code reviews of other engineers.
+        </p>
+      </span>
       <img className="ch-logo" src={changeLogo} alt="Change logo" />
     </div>
     <div className="ch-sub-content">
