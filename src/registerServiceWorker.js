62a70f1f6ed1b24bc9e5fabba888a174154f3134
William Grosset <williamhgrosset@gmail.com>
2018-03-12 19:14:41 -0700

    Nit: eslint ignore prod file

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -12,7 +13,8 @@ const isLocalhost = Boolean(window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),);
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
+);
 
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
@@ -36,8 +38,7 @@ export default function register() {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log('This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://goo.gl/SC7cgQ',
-          );
+              'worker. To learn more, visit https://goo.gl/SC7cgQ');
         });
       } else {
         // Is not local host. Just register service worker
