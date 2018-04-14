1dfa4ed7437c0bd3dee3ece0a3c600590af8c0e0
William Grosset <williamhgrosset@gmail.com>
2018-04-14 16:28:03 -0700

    Animate Project height if inside viewport

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,24 +7,36 @@ import '../styles/components/Project.css';
 class Project extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.consoleLog = this.consoleLog.bind(this);
+    this.state = {
+      isInWaypoint: false,
+    };
+    this.showProject = this.showProject.bind(this);
+    this.hideProject = this.hideProject.bind(this);
   }
 
-  consoleLog() {
-    console.log('entered');
+  showProject() {
+    this.setState({
+      isInWaypoint: true,
+    });
+  }
+
+  hideProject() {
+    this.setState({
+      isInWaypoint: false,
+    });
   }
 
   render() {
     const { title, subtitle, description, details, source, windowHeight } = this.props;
+    const { isInWaypoint } = this.state;
 
     return (
-      <Waypoint onEnter={this.consoleLog}>
-        <div className="project fade-in">
+      <Waypoint onEnter={this.showProject} onLeave={this.hideProject}>
+        <div className="project">
           <div className="project-content">
             <p className="project-title">{title}</p>
             <hr className="project-hr" />
-            <div className="project-primary">
+            <div className={`project-primary ${isInWaypoint ? 'project-primary-enter' : 'project-primary-leave'}`}>
               <p className="project-subtitle">{subtitle}</p>
               <p className="project-desc" dangerouslySetInnerHTML={{ __html: description }} />
               <ProjectDetailsList detailsList={details} sourceURL={source} />
