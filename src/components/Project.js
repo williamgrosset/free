62fe7accdad158f77ef3219db3eaeaa3deec7fbe
William Grosset <williamhgrosset@gmail.com>
2018-04-15 13:26:43 -0700

    Add linting rule reasons

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-danger */
+/* (only static content) */
 import React, { Component } from 'react';
 import Waypoint from 'react-waypoint';
 import PropTypes from 'prop-types';
