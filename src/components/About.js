18850c9a1c6e7d65797b50d07406d72d4dfb595a
William Grosset <williamhgrosset@gmail.com>
2018-04-15 22:33:05 -0700

    Remove middle name

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import '../styles/components/About.css';
 const About = () => (
   <div className="about">
     <p className="about-desc about-media-content fade-in">
-      My name is William Hamilton Grosset.<br />
+      My name is William Grosset.<br />
       Currently, I am studying Computer Science at the University of Victoria.<br />
       Previously, I worked as a research assistant at the{' '}
       <a className="royal-link" href="https://en.wikipedia.org/wiki/University_of_Victoria">
