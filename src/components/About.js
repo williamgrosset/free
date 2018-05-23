1d3ff00e4b283e979f58407b951d677b78ba5bda
William Grosset <williamhgrosset@gmail.com>
2018-05-23 12:29:23 +0100

    Update university href link

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,10 @@ const About = () => (
   <div className="about">
     <p className="about-desc about-desc-media fade-in">
       My name is William Grosset.<br />
-      Currently, I am studying Computer Science at the University of Victoria. Previously, I worked as a research
-      assistant at the{' '}
+      Currently, I am studying Computer Science at the{' '}
       <a className="royal-link" href="https://en.wikipedia.org/wiki/University_of_Victoria">
-        University
-      </a>{' '}
-      and as a software engineer at{' '}
+        University of Victoria
+      </a>. Previously I worked as a research assistant at the University and as a software engineer at{' '}
       <a className="royal-link" href="https://www.change.org">
         Change.org
       </a>.
