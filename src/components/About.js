887e203da937669812c6238361dc0b9aa36bfeaf
William Grosset <williamhgrosset@gmail.com>
2018-08-12 12:52:18 -0700

    Re-order About summary

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,16 @@ const About = () => (
     <p className="about-desc about-desc-media fade-in">
       <p className="about-name about-name-media">William Grosset.</p>
       <span className="about-desc-sub">
+        Creator of{' '}
+        <a className="royal-link" href="https://github.com/williamgrosset/chapter">
+          Chapter
+        </a>.<br />
         Computer Science student at the{' '}
         <a className="royal-link" href="https://en.wikipedia.org/wiki/University_of_Victoria">
           University of Victoria
         </a>.<br />Previously research assistant and software engineer at{' '}
         <a className="royal-link" href="https://www.change.org">
           Change.org
-        </a>.<br />Creator of{' '}
-        <a className="royal-link" href="https://github.com/williamgrosset/chapter">
-          Chapter
         </a>.
       </span>
     </p>
