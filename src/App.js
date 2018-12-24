d3df79b304fe0b6497ec3689294760f30005054a
William Grosset <williamhgrosset@gmail.com>
2018-12-23 23:31:24 -0700

    Convert App from stateless to stateful component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import About from './components/About';
 import Project from './components/Project';
 import Contact from './components/Contact';
 import Spacer from './components/Spacer';
 import projects from './data/projects.json';
 
-const App = () => (
-  <div className="page">
-    <About />
-    <Spacer height={10} />
-    {projects.map(project => (
-      <Project
-        title={project.title}
-        subtitle={project.subtitle}
-        description={project.description}
-        details={project.details}
-        source={project.source}
-      />
-    ))}
-    <Spacer height={11} />
-    <Contact />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  trackClickEvent() {
+    window.analytics.track('Test event');
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <About />
+        <Spacer height={10} />
+        {projects.map(project => (
+          <Project
+            title={project.title}
+            subtitle={project.subtitle}
+            description={project.description}
+            details={project.details}
+            source={project.source}
+          />
+        ))}
+        <Spacer height={11} />
+        <Contact />
+      </div>
+    );
+  }
+}
 
 export default App;
